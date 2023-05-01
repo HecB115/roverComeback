732be771d9a262b67387b0c8a79cb7835c43d2e9
HecB115 <114847032+HecB115@users.noreply.github.com>
2023-05-01 01:06:39 -0700

    Giroscopio
    Solo agregue la condicion del giroscopio que me sirvio el concurso pasado para determinar si el rover ya esta estatico o si sigue en bajada. Ademas de unos pequenos cambios que considero que le dan mas estetica al codigo, pero eso ya es subjetivo jaja

--- Main/elevaciones.ino
+++ Main/elevaciones.ino
@@ -1,7 +1,7 @@
 
 void elevationCheck() {
   
-  if (alturaRelativa + 3 > alturaMaxima && banderaElevacionPositiva == false && banderaAterrizaje == false) {
+  if (alturaRelativa + 3.0 > alturaMaxima && banderaElevacionPositiva == false && banderaAterrizaje == false) {
     alturaMaxima = alturaRelativa;
     contadorAscenso++;
     if (contadorAscenso > 4){
@@ -9,7 +9,7 @@ void elevationCheck() {
     alturaMaxima = alturaRelativa;}
   }
 
-  if (banderaElevacionPositiva == true && alturaRelativa < alturaMaxima - 3 && banderaElevacionNegativa == false && banderaAterrizaje == false) {
+  if (banderaElevacionPositiva == true && alturaRelativa < alturaMaxima - 3.0 && banderaElevacionNegativa == false && banderaAterrizaje == false) {
     contadorDescenso++;
     alturaMaxima = alturaRelativa;
     if (contadorDescenso > 4){
@@ -17,13 +17,19 @@ void elevationCheck() {
     }
   }
 
-  if (banderaElevacionPositiva == true && banderaElevacionNegativa == true && alturaRelativa < 0.0 + 4 && alturaRelativa > 0.0 - 4 && banderaAterrizaje == false){
+  if (banderaElevacionPositiva == true && banderaElevacionNegativa == true && alturaRelativa < 4.0 && alturaRelativa > - 4.0 && banderaAterrizaje == false){
     banderaAterrizaje = true;
   }
 
-  if (banderaAterrizaje == true && banderaMovimiento == false) {
-    banderaMovimiento == true;
-    delay(10000);
+  /*Adicion de lecturas por medio de giroscopio para determinar si el rover ya no esta en movimiento
+    debido a la caida*/
+  if (banderaAterrizaje == true && abs(Gx) <= 0.5 && abs(Gy) <= 0.5 && abs(Gz) <= 0.5) {
+    banderaGiroscopio = true;
   }
-
+  
+  /*Al cumplirse la condicion anterior se define que el rover esta listo para moverse
+    al equivaler la bandera de movimiento como verdadera*/
+  if (banderaGiroscopio == true && banderaMovimiento == false){
+    banderaMovimiento = true;
+  }
 }
